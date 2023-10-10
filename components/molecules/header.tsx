261f7e4aa4feafda96dfa80f7c00be073ede6640
Praisecodes <cmancoder@gmail.com>
2023-10-10 02:29:57 +0100

    [format] just code formatting

--- components/molecules/header.tsx
+++ components/molecules/header.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import { Text, View } from "react-native";
 import tw from 'twrnc';
 
-const Header = ():React.ReactNode => {
+const Header = (): React.ReactNode => {
   const [firstName, setFirstName] = useState<string>("Joseph");
 
   return (
     <View style={tw`py-3 flex flex-row items-center`}>
       <View style={tw`bg-[#D9D9D9] w-[5rem] h-[5rem] rounded-full`}></View>
-      <Text style={[tw`text-white text-xl mx-4`,{fontFamily: "Raleway_Regular"}]}>
+      <Text style={[tw`text-white text-xl mx-4`, { fontFamily: "Raleway_Regular" }]}>
         Hello, {firstName}!
       </Text>
     </View>
