727afa9b3f9e8ea81ad3e20580d47c2b9c5b0af5
Praisecodes <cmancoder@gmail.com>
2023-10-10 16:15:06 +0100

    [update] fix date on card

--- components/molecules/todoCard.tsx
+++ components/molecules/todoCard.tsx
@@ -7,13 +7,13 @@ const TodoCard = ({ todo }: { todo: any; }): React.ReactNode => {
   const lastName = userStore((state) => state.lastName);
 
   return (
-    <View style={[tw`w-[100%] bg-[#2C2B2B] p-4 rounded-2xl gap-6`]}>
+    <View style={[tw`w-[100%] bg-[#2C2B2B] p-4 rounded-2xl gap-7`]}>
       <View>
         <Text style={[tw`text-white text-xl`, {fontFamily: "Raleway-Bold"}]}>
           {todo?.title}
         </Text>
-        <Text style={[tw`text-lg text-[#A9A8A8]`, {fontFamily: "Raleway_Regular"}]}>
-          Due Date: <Text style={{fontFamily: "Raleway-Bold"}}>{new Date(todo?.dateDue).toLocaleString()}</Text>
+        <Text style={[tw`text-base text-[#A9A8A8]`, {fontFamily: "Raleway_Regular"}]}>
+          Due Date: <Text style={{fontFamily: "Raleway-Bold"}}>{new Date(todo?.dateDue).toDateString().split(" ")[1] + " " + new Date(todo?.dateDue).toDateString().split(" ")[2]}</Text>
         </Text>
       </View>
 
