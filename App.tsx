9dad8bca0c715f9c663084c2664f81296d1bc110
Praisecodes <cmancoder@gmail.com>
2023-10-10 08:56:15 +0100

    [fix] fix scrollbar

--- App.tsx
+++ App.tsx
@@ -49,29 +49,30 @@ function App(): JSX.Element {
 
   return (
     <GestureHandlerRootView>
-      <SafeAreaView style={tw`bg-[#414045] px-4 min-h-[100%]`}>
+      <SafeAreaView style={tw`bg-[#414045] px-4 h-[100%]`}>
         <StatusBar
           barStyle={'light-content'}
           backgroundColor={"#414045"}
         />
         <Header />
 
-        <ScrollView scrollEnabled contentContainerStyle={[tw`py-2 w-[100%]`]}>
-          <View style={[tw`flex gap-3 flex-row w-[100%] py-2`]}>
-            <FilterButton focus={done} onPress={() => { changeSelected("done") }}>
-              Done To-Do's
-            </FilterButton>
+        <ScrollView contentContainerStyle={[tw`py-2 w-[100%]`]}>
+          <View>
+            <View style={[tw`flex gap-3 flex-row w-[100%] py-2`]}>
+              <FilterButton focus={done} onPress={() => { changeSelected("done") }}>
+                Done To-Do's
+              </FilterButton>
 
-            <FilterButton focus={unDone} onPress={() => { changeSelected("undone") }}>
-              Undone To-Do's
-            </FilterButton>
+              <FilterButton focus={unDone} onPress={() => { changeSelected("undone") }}>
+                Undone To-Do's
+              </FilterButton>
+            </View>
+
+            <ReminderSection />
+
+            <UpcomingSection bottomSheetRef={bottomSheetRef} />
           </View>
-
-          <ReminderSection />
-
-          <UpcomingSection bottomSheetRef={bottomSheetRef} />
         </ScrollView>
-
       </SafeAreaView>
 
       <BottomSheet backgroundStyle={[tw`bg-[#2C2B2B] rounded-t-3xl`]} ref={bottomSheetRef} enablePanDownToClose snapPoints={snapPoints} index={-1}>
